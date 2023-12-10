1f499952493aa93040e9f52a0cc4cd83fba82016
Nanitanai <t.nahir.correa@hotmail.com>
2023-12-10 00:21:31 -0300

    backend

---
+++ proyecto/target/classes/IPersistencia.java
@@ -0,0 +1,18 @@
+public class IPersistencia {
+    
+    // CRUD //
+    
+    //CREATE
+    //void guardar(Usuario newUsuario);
+    
+    //READ
+    //ArrayList<Usuario> listarUsuarios();
+    //Usuario getUsuarioPorId(String id);
+
+    //UPDATE
+    //Usuario update(String usuario);
+
+    //DELETE
+    //void delete(String id);
+    
+}
